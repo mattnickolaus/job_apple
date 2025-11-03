dad2860da8f99f92102c73b295ebbdee4cd45809
mattnickolaus <mattnickolaus@gmail.com>
2025-11-02 22:50:57 -0500

    adding really basic routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello there!"))
+	w.Write([]byte("Hello there! Welcome to Job Apple"))
+}
+
+func jobAppView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	msg := fmt.Sprintf("Display a specific job applicaiton with ID %d", id)
+	w.Write([]byte(msg))
+}
+
+func jobAppCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new job application..."))
+}
+
+func jobAppCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(201)
+	w.Write([]byte("Save a new snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /jobapp/view/{id}", jobAppView)
+	mux.HandleFunc("GET /jobapp/create", jobAppCreate)
+	mux.HandleFunc("POST /jobapp/create", jobAppCreatePost)
 
 	log.Printf("starting server on :4000")
 
