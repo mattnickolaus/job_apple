4fda3dab10ab71b3f3288d03f06286f610127a8b
mattnickolaus <mattnickolaus@gmail.com>
2025-11-06 17:03:38 -0500

    Beginning to add html templating

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,38 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Server", "Go")
-
-	w.Write([]byte("Hello there! Welcome to Job Apple"))
-}
-
-func jobAppView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
-
-func jobAppCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new job application..."))
-}
-
-func jobAppCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Save a new snippet"))
-}
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileSever := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileSever))
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /jobapp/view/{id}", jobAppView)
 	mux.HandleFunc("GET /jobapp/create", jobAppCreate)
